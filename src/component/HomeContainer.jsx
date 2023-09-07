647cd29a5ddb05305c8cf6788213e0021bac90c5
DanielAgbeni <danielagbeni12@gmail.com>
2023-09-07 12:47:48 +0100

    update

--- src/component/HomeContainer.jsx
+++ src/component/HomeContainer.jsx
@@ -37,15 +37,12 @@ const HomeContainer = () => {
 					<TypingAnimation />
 				</div>
 				<p className='text-base text-textColor text-center md:text-left md:w-[80%]'>
-					Lorem ipsum dolor sit amet consectetur adipisicing elit. Quos impedit
-					ut ab. Cum debitis optio dolor accusantium adipisci fugiat, ipsa
-					itaque laboriosam dolores rem molestias, omnis illo exercitationem
-					quas nobis assumenda dicta libero possimus ipsum! Aspernatur quia
-					tempora odit magnam?
+					The fastest food delivery within your city and delivery at your
+					doorstep
 				</p>
-				<button className='bg-gradient-to-br from-orange-400 to-orange-500 w-full md:w-auto px-4 py-2  rounded-lg hover:shadow-lg transition-all ease-in-out duration-100'>
+				{/* <button className='bg-gradient-to-br from-orange-400 to-orange-500 w-full md:w-auto px-4 py-2  rounded-lg hover:shadow-lg transition-all ease-in-out duration-100'>
 					Order Now
-				</button>
+				</button> */}
 			</div>
 			<div className='py-2 flex-1 flex items-center relative'>
 				<img
