afe95e09b9c147babb9c75bca7bc4cee14fd09f9
DanielAgbeni <danielagbeni12@gmail.com>
2023-09-07 23:19:22 +0100

    fix padding issue

--- src/component/About.jsx
+++ src/component/About.jsx
@@ -5,11 +5,11 @@ import React from 'react'
 const About = () => {
 	return (
 		<div className='w-full h-auto flex flex-col items-center justify-center'>
-			<div className='mx-80px p-9 flex flex-col items-center justify-center'>
+			<div className='mx-0 p-0 flex flex-col items-center justify-center md:p-0'>
 				<h2 className='text-4xl font-semibold capitalize mb-10 text-headingColor relative before:absolute before:rounded-lg before:content before:w-32 before:h-1 before:-bottom-2 before:left-[50%] before:mx-auto before:-translate-x-[50%]  before:bg-gradient-to-tr from-orange-400 to-orange-600 transition-all ease-in-out duration-100'>
 					About Food Mama
 				</h2>
-				<p className='mx-80px  px-24 text-center text-xl font-normal mb-6 md:mx-1'>
+				<p className='lg:mx-80px  px-4 text-center text-xl font-normal mb-6 md:mx-1'>
 					Welcome to Food Mama, a culinary haven where exquisite flavors and
 					heartwarming dishes come together to create unforgettable dining
 					experiences. Nestled in the heart of Lagos Nigeria, Food Mama is not
@@ -19,14 +19,14 @@ const About = () => {
 				<h2 className='text-4xl font-semibold capitalize mb-10 text-headingColor relative before:absolute before:rounded-lg before:content before:w-32 before:h-1 before:-bottom-2 before:left-[50%] before:mx-auto before:-translate-x-[50%]  before:bg-gradient-to-tr from-orange-400 to-orange-600 transition-all ease-in-out duration-100'>
 					Our Story
 				</h2>
-				<p className='mx-80px  px-24 text-center text-xl font-normal md:mx-1'>
+				<p className='mx-80px  px-4 text-center text-xl font-normal md:mx-1'>
 					Food Mama is the brainchild of Chef Maria Rodriguez, whose passion for
 					cooking was nurtured in her grandmother's kitchen. Inspired by the
 					delicious, comforting meals her "Mama" used to prepare, Chef Maria
 					embarked on a culinary journey to bring those cherished flavors to a
 					wider audience.
 				</p>
-				<p className='mx-80px  px-24 text-center text-xl font-normal mb-6 md:mx-1'>
+				<p className='mx-80px  px-4 text-center text-xl font-normal mb-6 md:mx-1'>
 					Our restaurant is a tribute to those cherished family recipes, where
 					each dish tells a story of tradition, innovation, and an unwavering
 					commitment to quality. At Food Mama, we believe that food is more than
@@ -36,13 +36,13 @@ const About = () => {
 				<h2 className='text-4xl font-semibold capitalize mb-10 text-headingColor relative before:absolute before:rounded-lg before:content before:w-32 before:h-1 before:-bottom-2 before:left-[50%] before:mx-auto before:-translate-x-[50%]  before:bg-gradient-to-tr from-orange-400 to-orange-600 transition-all ease-in-out duration-100'>
 					The Food Experience
 				</h2>
-				<p className='mx-80px  px-24 text-center text-xl font-normal md:mx-1'>
+				<p className='mx-80px  px-4 text-center text-xl font-normal md:mx-1'>
 					Our menu at Food Mama is a carefully curated selection of dishes that
 					cater to diverse tastes and dietary preferences. From sizzling grilled
 					meats and fresh seafood to flavorful vegetarian and vegan options,
 					there's something for everyone.
 				</p>
-				<p className='mx-80px  px-24 text-center text-xl font-normal mb-6 md:mx-1'>
+				<p className='mx-80px  px-4 text-center text-xl font-normal mb-6 md:mx-1'>
 					Our chefs source the finest ingredients locally, ensuring that each
 					dish bursts with freshness and authenticity. Whether you're craving a
 					hearty plate of Mama's famous lasagna, a zesty Thai curry, or a
@@ -51,7 +51,7 @@ const About = () => {
 				<h2 className='text-4xl font-semibold capitalize mb-10 text-headingColor relative before:absolute before:rounded-lg before:content before:w-32 before:h-1 before:-bottom-2 before:left-[50%] before:mx-auto before:-translate-x-[50%]  before:bg-gradient-to-tr from-orange-400 to-orange-600 transition-all ease-in-out duration-100'>
 					Warm and Welcoming Atmosphere
 				</h2>
-				<p className='mx-80px  px-24 text-center text-xl font-normal mb-6 md:mx-1'>
+				<p className='mx-80px  px-4 text-center text-xl font-normal mb-6 md:mx-1'>
 					Step into Food Mama, and you'll instantly feel at home. Our cozy and
 					inviting ambiance is designed to make you feel like part of the
 					family. Whether you're dining with loved ones or enjoying a solo meal,
@@ -61,7 +61,7 @@ const About = () => {
 				<h2 className='text-4xl font-semibold capitalize mb-10 text-headingColor relative before:absolute before:rounded-lg before:content before:w-32 before:h-1 before:-bottom-2 before:left-[50%] before:mx-auto before:-translate-x-[50%]  before:bg-gradient-to-tr from-orange-400 to-orange-600 transition-all ease-in-out duration-100'>
 					Community and Sustainability
 				</h2>
-				<p className='mx-80px  px-24 text-center text-xl font-normal mb-6 md:mx-1'>
+				<p className='mx-80px  px-4 text-center text-xl font-normal mb-6 md:mx-1'>
 					At Food Mama, we believe in giving back to the community that has
 					embraced us. We actively support local farmers, artisans, and
 					charities, ensuring that our commitment to sustainability goes beyond
@@ -70,7 +70,7 @@ const About = () => {
 				<h2 className='text-4xl font-semibold capitalize mb-10 text-headingColor relative before:absolute before:rounded-lg before:content before:w-32 before:h-1 before:-bottom-2 before:left-[50%] before:mx-auto before:-translate-x-[50%]  before:bg-gradient-to-tr from-orange-400 to-orange-600 transition-all ease-in-out duration-100'>
 					Join Us at Food Mama
 				</h2>
-				<p className='mx-80px  px-24 text-center text-xl font-normal mb-6  md:mx-1'>
+				<p className='mx-80px  px-4 text-center text-xl font-normal mb-6  md:mx-1'>
 					Whether you're a food enthusiast, a family looking for a cozy dinner,
 					or a group seeking a memorable dining experience, Food Mama is here to
 					welcome you with open arms and delicious food. Come and indulge in the
